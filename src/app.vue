0849afec99f8898a869f9cf4223f5c075ae04607
dengbohua <deng.bohua@h3c.com>
2018-07-31 17:01:33 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+﻿<template>
+    <div class="msg-wrapper">
+        <span>hello!</span>
+        <span class="account">{{data.name}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Msg',
+        data () {
+            return {}
+        },
+        props:{
+            data:{
+                type:Object
+            }
+        },
+        methods: {
+ 
+        },
+        created(){
+            let that = this;
+        }
+    }
+</script>
+
+<style scoped lang="css">
+    
+</style>
